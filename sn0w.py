08cdf344359df2fa498b54101f0fbdb7911763ce
s0ph0s <54l0m0n@users.noreply.github.com>
2016-04-27 15:59:28 -0300

    Sniffer

--- sn0w.py
+++ sn0w.py
@@ -3,33 +3,52 @@
 from scapy.all import *
 from scapy.error import Scapy_Exception
 
-interface_rede = raw_input ("Digite sua interface de rede: ") # wlan, eth..
-filter_message=raw_input("Tipo de pacote: ") #ftp, ssh, http, any(para todos os tipos de pacotes)
-arquivo = open("sniffer.txt", "a") # gerando log. do arquivo
+print """"
+  ██████  ███▄    █  ▒█████   █     █░
+▒██    ▒  ██ ▀█   █ ▒██▒  ██▒▓█░ █ ░█░
+░ ▓██▄   ▓██  ▀█ ██▒▒██░  ██▒▒█░ █ ░█ 
+  ▒   ██▒▓██▒  ▐▌██▒▒██   ██░░█░ █ ░█ 
+▒██████▒▒▒██░   ▓██░░ ████▓▒░░░██▒██▓ 
+▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▓░▒ ▒  
+░ ░▒  ░ ░░ ░░   ░ ▒░  ░ ▒ ▒░   ▒ ░ ░  
+░  ░  ░     ░   ░ ░ ░ ░ ░ ▒    ░   ░  
+      ░           ░     ░ ░      ░  
+"""
 
-def sniffer_TCP(snin):
-        global count
-        if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 21 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta FTP          
-                print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 22 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta SSH
-                print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 23 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta TELNET    
-                print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 25 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta SMTP         
-                print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 80 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta HTTP         
+net_interface = raw_input ("Insert your network interface: ")
+filter_message=raw_input("Insert the protocol: ")
+arquivo = open("sniffer_output", "a") # gerando log. do arquivo
+
+def s0ph0s_TCP(snin):
+        if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 21 and snin.haslayer(Raw): # FTP service          
                 print snin.getlayer(Raw).load
 		arquivo.write("%s \r\n" % snin.getlayer(Raw))
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 110 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta POP3         
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 22 and snin.haslayer(Raw): # SSH service
                 print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 143 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta IMAP        
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 23 and snin.haslayer(Raw): # TELNET service    
                 print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 133 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta IRC       
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 25 and snin.haslayer(Raw): # SMTP service         
                 print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 161 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta SNMP  
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 80 and snin.haslayer(Raw): # HTTP service         
                 print snin.getlayer(Raw).load
-	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 194 and snin.haslayer(Raw): #pega a camada/pacotes TCP rodando na porta IRC       
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 110 and snin.haslayer(Raw): # POP3 service         
                 print snin.getlayer(Raw).load
-sniff(iface=interface_rede, prn=sniffer_TCP, store=0) #sniff(função) / iface = interface de rede / store = 0 nao acumula na memoria
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 143 and snin.haslayer(Raw): # IMAP service        
+                print snin.getlayer(Raw).load
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 133 and snin.haslayer(Raw): # IRC service       
+                print snin.getlayer(Raw).load
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 161 and snin.haslayer(Raw): # SNMP service
+                print snin.getlayer(Raw).load
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+	if snin.haslayer(TCP) and snin.getlayer(TCP).dport == 194 and snin.haslayer(Raw): # IRC service       
+                print snin.getlayer(Raw).load
+		arquivo.write("%s \r\n" % snin.getlayer(Raw))
+sniff(iface=net_interface, prn=s0ph0s_TCP, store=0) # store = 0; not allocate in memory 
 arq.close() 
-
